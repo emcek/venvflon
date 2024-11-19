bb0789f945502fc88cd6cfeaa52b2c104738085f
Michał Plichta <michal.plichta@pl.abb.com>
2024-11-19 13:57:53 +0100

    fix unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,13 +17,13 @@ def test_run_command(cmd, result):
 def test_success_deep_1_venv_list_in(resources):
     venvs = utils.venv_list_in(current_path=resources, max_depth=1)
     assert len(venvs) == 3
-    assert [venv.name for venv in venvs] == ['.venv_310', '.venv_311', '.venv_312']
+    assert sorted([venv.name for venv in venvs]) == ['.venv_310', '.venv_311', '.venv_312']
 
 
 def test_success_deep_2_venv_list_in(resources):
     venvs = utils.venv_list_in(current_path=resources, max_depth=2)
     assert len(venvs) == 4
-    assert [venv.name for venv in venvs] == ['.venv_310', '.venv_311', '.venv_312', '.venv_39']
+    assert sorted([venv.name for venv in venvs]) == ['.venv_310', '.venv_311', '.venv_312', '.venv_39']
 
 
 def test_failure_venv_list_in():
