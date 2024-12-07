b4cc58972605494f240a1085ab52f0183ca8ed42
Michal Plichta <mplichta@gmail.com>
2024-12-07 19:05:00 +0100

    add new UT

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,6 +12,19 @@ def test_run_command(cmd, result):
     rc = utils.run_command(cmd=['powershell', cmd])
     assert rc == result
 
+def test_get_command_output_success():
+    rc, err, out = utils.get_command_output(cmd=['python', '-V', '-V'])
+    assert rc == 0
+    assert err == ''
+    assert 'Python 3.' in out
+
+
+def test_get_command_output_failure():
+    rc, err, out = utils.get_command_output(cmd=['python', '-fake'])
+    assert rc == 2
+    assert 'Unknown option: -f' in err
+    assert out == ''
+
 
 def test_success_deep_1_venv_list_in(resources):
     venvs = utils.venv_list_in(current_path=resources, max_depth=1)
