55d4a642242975c21dd7f89c2abb0b79aa734d6d
Michał Plichta <michal.plichta@pl.abb.com>
2024-12-08 16:52:13 +0100

    release 0.4.0

--- src/venvflon/run.py
+++ src/venvflon/run.py
@@ -11,7 +11,7 @@ environ['TCL_LIBRARY'] = str(Path(base_prefix) / 'tcl' / 'tcl8.6')
 environ['TK_LIBRARY'] = str(Path(base_prefix) / 'tcl' / 'tk8.6')
 import tkinter as tk
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 
 def run():
