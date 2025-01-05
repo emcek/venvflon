91837b75efd2efad1548287eac4641c1576380cd
Michal Plichta <mplichta@gmail.com>
2025-01-06 00:19:04 +0100

    Update version 0.6.2

--- src/venvflon/flon.py
+++ src/venvflon/flon.py
@@ -10,7 +10,7 @@ from tkinterdnd2 import DND_FILES, TkinterDnD
 
 from venvflon import utils
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 KEYWORDS = ('Resolved', 'Installed', 'Audited', 'Prepared', 'Uninstalled')
 
 
