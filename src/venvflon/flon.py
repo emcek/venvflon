27fe280ba0a4f462ba2a05fb97092823119e4c8f
Michal Plichta <mplichta@gmail.com>
2025-04-07 00:17:25 +0200

    update version to 0.7.0

--- src/venvflon/flon.py
+++ src/venvflon/flon.py
@@ -12,7 +12,7 @@ from tkinterdnd2 import DND_FILES, TkinterDnD
 
 from venvflon import utils
 
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 KEYWORDS = ('Resolved', 'Installed', 'Audited', 'Prepared', 'Uninstalled')
 LATEST_PYTHON = '3.13'
 
